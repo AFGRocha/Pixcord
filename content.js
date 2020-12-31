7da5fbe2ac04b46ad874e2559903b03715933f15
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-12-31 13:15:53 +0000

    Change DOM selectors to prevent breaking when pixiv changes

--- content.js
+++ content.js
@@ -9,96 +9,111 @@ chrome.runtime.onMessage.addListener(function (res) {
             else if (type == "nsfw")
             button = document.getElementById("PixcordNSFW")
 
-            button.innerHTML = '<img src="https://i.imgur.com/4LBBzRr.gif">'
-            var request = new XMLHttpRequest();
-            request.open("POST", webhook);
+    var request = new XMLHttpRequest();
+    request.open('POST', webhook);
 
-            var xhr = new XMLHttpRequest();
-            var xhr2 = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
+    var xhr2 = new XMLHttpRequest();
 
+    //Art upload settings
+    xhr.open('POST', 'https://api.imgur.com/3/image');
+    xhr.setRequestHeader('Authorization', 'Client-ID d2906dca59f16f0');
 
+    var dataFields = new FormData();
 
-            //Art upload settings
-            xhr.open("POST", "https://api.imgur.com/3/image");
-            xhr.setRequestHeader('Authorization', 'Client-ID d2906dca59f16f0');
+    var imgSrc = document
+      .querySelector('.gtm-expand-full-size-illust')
+      .querySelector('img').src;
 
-            var dataFields = new FormData();
-            dataFields.append('image', document.getElementsByClassName('sc-1qpw8k9-1 fvHoJ')[0].src);
-            dataFields.append('type', 'URL');
-            xhr.send(dataFields)
+    // Other methods of getting image source in case something breaks
+    // document.querySelectorAll('[role="presentation"]')[2]?.querySelector('img').src
+    //document.getElementsByClassName('sc-1qpw8k9-1 fvHoJ')[0].src;
 
-            //Profile picture upload settings
-            xhr2.open("POST", "https://api.imgur.com/3/image");
-            xhr2.setRequestHeader('Authorization', 'Client-ID d2906dca59f16f0');
+    dataFields.append('image', imgSrc);
+    dataFields.append('type', 'URL');
+    xhr.send(dataFields);
 
+    //Profile picture upload settings
+    xhr2.open('POST', 'https://api.imgur.com/3/image');
+    xhr2.setRequestHeader('Authorization', 'Client-ID d2906dca59f16f0');
 
-            let firstStep = document.getElementsByClassName('sc-1asno00-0 iyBsWP')[0].innerHTML.replace('<img src="', '');
-            let profileImg = firstStep.replace('" width="40" height="40" alt="' + document.getElementsByClassName('sc-fzozJi iFrcHJ')[0].innerHTML + '" style="object-fit: cover; object-position: center top;">', '')
-            var dataFields2 = new FormData();
-            dataFields2.append('image', profileImg);
-            dataFields2.append('type', 'URL');
-            let artTitle = "Untitled"
+    // let firstStep = document
+    //   .getElementsByClassName('sc-1asno00-0 iyBsWP')[0]
+    //   .innerHTML.replace('<img src="', '');
+    // let profileImg = firstStep.replace(
+    //   '" width="40" height="40" alt="' +
+    //     document.getElementsByClassName('sc-fzozJi daCWkW')[0].innerText +
+    //     '" style="object-fit: cover; object-position: center top;">',
+    //   ''
+    // );
 
-            if(document.getElementsByClassName('sc-1u8nu73-3')[0])
-                artTitle = document.getElementsByClassName('sc-1u8nu73-3')[0].innerHTML
+    const profileLinks = document
+      .querySelector('aside')
+      .querySelectorAll('a[href^="/en/users/"]');
+    const profileImg = profileLinks[0].querySelector('img').src;
+    const profileName = profileLinks[1].innerHTML;
 
-            xhr.onloadend = function (evt) {
-                var responseJSON = JSON.parse(xhr.responseText);
+    var dataFields2 = new FormData();
+    dataFields2.append('image', profileImg);
+    dataFields2.append('type', 'URL');
 
+    const artDescription = document.querySelector('figcaption');
+    const artTitle =
+      artDescription.querySelector('h1')?.innerHTML || 'Untitled';
 
-                xhr2.send(dataFields2)
-                xhr2.onloadend = function (evt) {
-                    var profileJSON = JSON.parse(xhr2.responseText);
+    xhr.onloadend = function (evt) {
+      var responseJSON = JSON.parse(xhr.responseText);
 
-                    request.setRequestHeader('Content-type', 'application/json');
-                    //get name and profile pic
-                    chrome.storage.sync.get({
-                        name: 'Pixcord',
-                        discordImage: 'https://i.imgur.com/VQFaBcn.png'
-                    }, function (items) {
+      xhr2.send(dataFields2);
+      xhr2.onloadend = function (evt) {
+        var profileJSON = JSON.parse(xhr2.responseText);
 
-                        var params = {
-                            username: items.name,
-                            avatar_url: items.discordImage,
-                            content: "",
-                            embeds: [
-                                {
-                                    "title": artTitle ,
-                                    "url": location.href,
-                                    "color": 7506394,
-                                    "author": {
-                                        "name": document.getElementsByClassName('sc-fzozJi iFrcHJ')[0].innerHTML,
-                                        "url": document.getElementsByClassName('sc-fzozJi iFrcHJ')[0].href,
-                                        "icon_url": profileJSON.data.link
-                                    },
-                                    "footer": {
-                                        "text": "Pixcord developed by @DaikiPT"
-                                    },
-                                    "timestamp": new Date(),
-                                    "image": {
-                                        "url": responseJSON.data.link
-                                    }
-                                }
-                            ]
-                        }
-                        request.send(JSON.stringify(params));
-                        button.innerHTML = '✔️Shared'
-
-
-                    })
-
-
+        request.setRequestHeader('Content-type', 'application/json');
+        //get name and profile pic
+        chrome.storage.sync.get(
+          {
+            name: 'Pixcord',
+            discordImage: 'https://i.imgur.com/VQFaBcn.png'
+          },
+          function (items) {
+            var params = {
+              username: items.name,
+              avatar_url: items.discordImage,
+              content: '',
+              embeds: [
+                {
+                  title: artTitle,
+                  url: location.href,
+                  color: 7506394,
+                  author: {
+                    profileName,
+                    url: profileLinks[0].href,
+                    icon_url: profileJSON.data.link
+                  },
+                  footer: {
+                    text: 'Pixcord developed by @DaikiPT'
+                  },
+                  timestamp: new Date(),
+                  image: {
+                    url: responseJSON.data.link
+                  }
                 }
-
-
+              ]
             };
+            request.send(JSON.stringify(params));
+            button ? (button.innerHTML = '✔️Shared') : null;
+          }
+        );
+      };
+    };
 
-            xhr.onerror = function (err) { // untested
-                //console.log('Failed uploading image from ' + fileName + ': ' + JSON.stringify(err));
-                //console.log('Output: ' + xhr.responseText);
-                alert("Pixcord Upload Error")
-            };
-        }
+    xhr.onerror = function (err) {
+      // untested
+      //console.log('Failed uploading image from ' + fileName + ': ' + JSON.stringify(err));
+      //console.log('Output: ' + xhr.responseText);
+      alert('Pixcord Upload Error');
+    };
+  }
 
         if(!document.getElementById("PixcordSFW")){
             //Creating the first div
