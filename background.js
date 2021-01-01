b08809b7be037696b28c3abde1aa14c30350eaf4
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2021-01-01 03:55:53 +0000

    Function to get LocalStorage values

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(changeInfo);
-  setTimeout(function () {
-    console.log('loaded');
-    chrome.tabs.sendMessage(tabId, 'discord');
-  }, 2000);
-});
+async function getLocalStorageValue(key) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.sync.get(key, function (value) {
+        if (!value[key]) {
+          if (key === 'name') {
+            resolve('Pixcord');
+          } else if (key === 'discordImage')
+            resolve('https://i.imgur.com/VQFaBcn.png');
+        }
+        resolve(value[key]);
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+}
