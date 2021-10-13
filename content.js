4980ddd45e16e5b688bedaffed7f3d73db10b01e
António Rocha <afgrocha98@gmail.com>
2021-10-14 00:03:36 +0100

    query selectors

--- content.js
+++ content.js
@@ -25,7 +25,7 @@ chrome.runtime.onMessage.addListener(function (res) {
             xhr.setRequestHeader('Authorization', 'Client-ID d2906dca59f16f0');
 
             var dataFields = new FormData();
-            dataFields.append('image', document.getElementsByClassName('sc-1qpw8k9-1 jOmqKq')[0].src);
+            dataFields.append('image', document.querySelector('main section figure img').src);
             dataFields.append('type', 'URL');
             xhr.send(dataFields)
 
@@ -33,9 +33,12 @@ chrome.runtime.onMessage.addListener(function (res) {
             xhr2.open("POST", "https://api.imgur.com/3/image");
             xhr2.setRequestHeader('Authorization', 'Client-ID d2906dca59f16f0');
 
+            const profileLinks = document
+            .querySelector('aside')
+            .querySelectorAll('a[href^="/en/users/"]');
 
-            let firstStep = document.getElementsByClassName('sc-1asno00-0 iyBsWP')[0].innerHTML.replace('<img src="', '');
-            let profileImg = firstStep.replace('" width="40" height="40" alt="' + document.getElementsByClassName('sc-AxmLO fOviRP')[0].children[0].innerHTML + '" style="object-fit: cover; object-position: center top;">', '')
+            let firstStep = profileLinks[0].querySelector('figure') || profileLinks[0].querySelector('img');
+            let profileImg = firstStep.src
             var dataFields2 = new FormData();
             dataFields2.append('image', profileImg);
             dataFields2.append('type', 'URL');
@@ -68,8 +71,8 @@ chrome.runtime.onMessage.addListener(function (res) {
                                     "url": location.href,
                                     "color": 7506394,
                                     "author": {
-                                        "name": document.getElementsByClassName('sc-AxmLO fOviRP')[0].children[0].innerHTML,
-                                        "url": document.getElementsByClassName('sc-AxmLO fOviRP')[0].href,
+                                        "name": profileLinks[1].innerText,
+                                        "url": profileLinks[0].href,
                                         "icon_url": profileJSON.data.link
                                     },
                                     "footer": {
@@ -103,35 +106,39 @@ chrome.runtime.onMessage.addListener(function (res) {
 
         if(!document.getElementById("PixcordSFW")){
             //Creating the first div
-            let divSFW = document.createElement("div");
-            divSFW.className = "sc-181ts2x-3 iujCSd"
-
+            let divSFW = document.createElement('div');
+            divSFW.className = 'sc-181ts2x-3 iujCSd';
 
             //SFW Button
-            let sfw = document.createElement("BUTTON");  // Create a <button> node
-            sfw.setAttribute("id", "PixcordSFW")
+            let sfw = document.createElement('BUTTON'); // Create a <button> node
+            sfw.setAttribute('id', 'PixcordSFW');
             sfw.onclick = function () {
-                artShare("https://discordapp.com/api/webhooks/547508568849383426/3ceqXPSXNmnEHikyR65GL0UHTJoASHZWeu49Re5IhBYszSjMwDv8hfspWFso_SoQ4SBI","sfw")
+            artShare("https://discordapp.com/api/webhooks/547508568849383426/3ceqXPSXNmnEHikyR65GL0UHTJoASHZWeu49Re5IhBYszSjMwDv8hfspWFso_SoQ4SBI","sfw")
             };
-            let t = document.createTextNode("Share to #art");      // Create a text node
-            sfw.appendChild(t);                                          // Append the text to <p>
+            let t = document.createTextNode('Share to #art'); // Create a text node
+            sfw.appendChild(t); // Append the text to <p>
             divSFW.appendChild(sfw);
-            document.getElementsByClassName("sc-181ts2x-0 gMEAWM")[0].appendChild(divSFW);           // Append to <div>
+            document
+            .getElementsByClassName('sc-181ts2x-0 gMEAWM')[0]
+            .appendChild(divSFW); // Append to <div>
 
             //Second div
-            let divNSFW = document.createElement("div");
-            divNSFW.className = "sc-181ts2x-3 iujCSd"
+            let divNSFW = document.createElement('div');
+            divNSFW.className = 'sc-181ts2x-3 iujCSd';
 
             //NSFW Button
-            let nsfw = document.createElement("BUTTON");  // Create a <button> node
-            nsfw.setAttribute("id", "PixcordNSFW")
+            let nsfw = document.createElement('BUTTON'); // Create a <button> node
+            nsfw.setAttribute('id', 'PixcordNSFW');
             nsfw.onclick = function () {
-                artShare("https://discordapp.com/api/webhooks/738107690672324639/GQeX-g04uhrHrLbxM4qV2E1ePRbUeXZd9xwX7lJCVcJDtrY-Bs3pSA15mm2cjoewQjxb","nsfw")
+            artShare("https://discordapp.com/api/webhooks/738107690672324639/GQeX-g04uhrHrLbxM4qV2E1ePRbUeXZd9xwX7lJCVcJDtrY-Bs3pSA15mm2cjoewQjxb","nsfw")
             };
-            let t2 = document.createTextNode("Share to #NSFW");      // Create a text node
-            nsfw.appendChild(t2);  // Append the text to <p>
-            divNSFW.appendChild(nsfw)
-            document.getElementsByClassName("sc-181ts2x-0 gMEAWM")[0].appendChild(divNSFW);           // Append to <div>
+            
+            let t2 = document.createTextNode('Share to #NSFW'); // Create a text node
+            nsfw.appendChild(t2); // Append the text to <p>
+            divNSFW.appendChild(nsfw);
+            document
+            .getElementsByClassName('sc-181ts2x-0 gMEAWM')[0]
+            .appendChild(divNSFW); // Append to <div>
 
             //Third Div
             let divNEGA = document.createElement("div");
